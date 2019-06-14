062c479e33013c4ad225cf085410b1458287a528
Sam <saudino480@gmail.com>
2019-06-13 22:57:24 -0400

    Updated date range to be a slider that displays month and year.

--- server.R
+++ server.R
@@ -5,22 +5,23 @@ library(tidyr)
 library(zoo)
 # use session and update selectizeINput to add global portfolios
 # use the tabsetPanel to add more panels
+
 function(input, output) {
   datasetInput <- reactive({
-    df = ps[ps$date>=as.yearmon(input$dates[1]) & 
-              ps$date<=as.yearmon(input$dates[2]),]
-    df[,c(-1)] = sapply(df[,c(-1)],  function(x){x/x[1]}) #use input$dates[1] as the initial investment date
-    melt_cumret=melt(df,id.vars = c('date'),measure.vars = colnames(df)[-1],variable.name = 'port_name',value.name = 'return')
+    
+    melt_cumret=melt(ps,id.vars = c('date'),measure.vars = colnames(ps)[-1],variable.name = 'port_name',value.name = 'return')
     melt_cumret[melt_cumret$port_name %in% c(input$old, input$new), ]
 
   })
   
-  output$count <- renderPlot(
+  output$count <- renderPlot({
+    
+    dates = input$slider
     
     ggplot(data = datasetInput()) +
-      geom_line(aes(x = date, y = return, color = port_name)) + 
-      ylab("Payoff to $1 investment") + scale_x_yearmon()
-  )
+      geom_line(aes(x = as.Date(date), y = return, color = port_name)) + 
+      ylab("Payoff to $1 investment") + xlim(dates)
+  })
   
   output$view <- renderTable({
     head(datasetInput())
--- ui.R
+++ ui.R
@@ -6,21 +6,28 @@ getwd()
 fluidPage(
   titlePanel("Infamous Fama-French Portfolios"),
   sidebarLayout(
+    
     sidebarPanel( 
       selectizeInput(
         inputId = "old", 
         label = "Benchmark Portfolio", 
         choices =  colnames(ps)[-1]
         ),
+      
       selectizeInput(
         inputId = "new", 
         label = "Smart Beta Strategies", 
         choices = colnames(ps[-1]) 
         ),
-      dateRangeInput(
-        "dates", 
-        label = ("Date range")
-        )
+      
+      sliderInput("slider", 
+                  "Time", 
+                  min = as.Date("1963-07-01"),
+                  max =as.Date("2019-04-01"),
+                  value=as.Date(c("1994-07-01", "2000-01-01")),
+                  timeFormat="%b %Y",
+                  width = "90%")
+  
     ),
     mainPanel(
       plotOutput("count"),
